56ef55b571366293a48b1b53fa71a8f6f61309d2
Gustavo-sou5a <gustavo.gomesdesousa@gmail.com>
2025-02-24 18:25:28 +0000

    Add the CSS and HTML that assembles the calculator appearance
    The buttons were all created in the .js file and
    its styles were directly made in the .css file, as
    well as for the rest of the calculator (display and
    outer part)

--- app.js
+++ app.js
@@ -29,18 +29,34 @@ console.log(operate(3, 4, divide));
  * Creating buttons for digits and operators on the DOM
  */
 const NUM_DIGITS = 10;
-const OPERATORS = ["+", "-", "*", "/", "=", "CLR"];
+const OPERATORS = ["AC", "+", "-", "*", "/", "="];
 
 const digitsDiv = document.querySelector("#digits");
-for (let i = 0; i < NUM_DIGITS; i++) {
+for (let i = NUM_DIGITS-1; i >= 0; i--) {
     let digitBtn = document.createElement("button");
     digitBtn.textContent = i;
+    digitBtn.classList.add(`digit-${i}`);
     digitsDiv.appendChild(digitBtn);
 }
+
+// Adding the CE (Clear Entry) button to erase the last input
+let clearBtn = document.createElement("button");
+clearBtn.textContent = "CE";
+clearBtn.classList.add("clear");
+digitsDiv.insertBefore(clearBtn, document.querySelector(".digit-0"));
+
+// Adding the decimal button "." for float numbers
+let decimalBtn = document.createElement("button");
+decimalBtn.textContent = ".";
+decimalBtn.classList.add("decimal");
+digitsDiv.appendChild(decimalBtn);
 
 const operatorsDiv = document.querySelector("#operators");
 OPERATORS.forEach((operator) => {
     let operatorBtn = document.createElement("button");
     operatorBtn.textContent = operator;
+    operatorBtn.setAttribute("id", `operator-${operator}`);
     operatorsDiv.appendChild(operatorBtn); 
 });
+
+
