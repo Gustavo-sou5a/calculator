1c9f1650d292212d7babf33e21b97d5495046c1a
Gustavo-sou5a <gustavo.gomesdesousa@gmail.com>
2025-02-25 18:56:34 +0000

    Calculator totally functional with the mouse
    It is already functional, however it is missing
    hover effect on the buttons and keyboard support
    as well

--- app.js
+++ app.js
@@ -14,33 +14,39 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    // ver caso onde b === 0
+    if (b === 0)
+        return "Crazy";
     return a / b;
 }
 
 function operate(a, b, operation) {
-
-    // é preciso passar para numeros, removendo os "." no final, se houver
+    switch (operation) {
+        case "+": operation = add; break;
+        case "-": operation = subtract; break;
+        case "*": operation = multiply; break;
+        case "/": operation = divide; break;
+    }
     return operation(a, b);
 }
-
-console.log(operate(3, 4, divide));
-
 
 /**
  * Creating buttons for digits and operators on the DOM
  */
 const NUM_DIGITS = 10;
-const OPERATORS = [{op:"AC", name:"ac"},
+// const MAX_DIGITS_PER_NUMBER = 12;
+const OPERATORS = [
+    {op:"AC", name:"ac"},
     {op:"+", name:"plus"},
     {op:"-", name:"minus"},
     {op:"*", name:"mult"},
     {op:"/", name:"div"},
-    {op:"=", name:"eq"}];
+    {op:"=", name:"eq"}
+];
 const EMPTY_OPERTATION = {
     a: NaN,
     op: NaN,
-    b: NaN
+    b: NaN,
+    lastOp: NaN
 };
 let runningOperation = {...EMPTY_OPERTATION};
 
@@ -50,7 +56,11 @@ for (let i = NUM_DIGITS-1; i >= 0; i--) {
     digitBtn.textContent = i;
     digitBtn.classList.add(`digit-${i}`);
     digitBtn.classList.add("digit");
-    digitBtn.addEventListener("click", () => addToDisplay(i));
+    digitBtn.addEventListener("click", () => {
+        // const display = document.querySelector("#display");
+        // if (display.textContent.length < MAX_DIGITS_PER_NUMBER)
+            addToDisplay(i);
+    });
     digitsDiv.appendChild(digitBtn);
 }
 
@@ -103,14 +113,20 @@ function addToDisplay(num) {
     enableOperatorButtons();
     const display = document.querySelector("#display");
 
-    if (!Number.isNaN(runningOperation.op) 
+    if (runningOperation.lastOp === "=" || display.textContent === "Crazy") {
+        if (num === ".")
+            display.textContent = "0" + num;
+        else
+            display.textContent = num;
+        runningOperation.lastOp = NaN;
+    }
+    else if (!Number.isNaN(runningOperation.op) 
         && Number.isNaN(runningOperation.b)) {
         if (num === ".")
             display.textContent = "0" + num;
         else
             display.textContent = num;
     }
-
     else if (display.textContent === "0" && num !== ".")
         display.textContent = num;
     else
@@ -120,6 +136,7 @@ function addToDisplay(num) {
 
     console.log(runningOperation.a);
     console.log(runningOperation.b);
+
 }
 
 /**
@@ -128,18 +145,20 @@ function addToDisplay(num) {
 function removeFromDisplay() {
     const display = document.querySelector("#display");
     let text = display.textContent;
-    display.textContent = text.slice(0, -1);
-
-    (Number.isNaN(runningOperation.op)) ? runningOperation.a = display.textContent : runningOperation.b = display.textContent;
-
-    if (text.length === 1) {
-        display.textContent = 0;
-        if (Number.isNaN(runningOperation.op))
-            disableOperatorButtons();
+    if (text === "Crazy") {
+        eraseData();
     }
-    else
-        clearBtn.disabled = false;
-
+    else {
+        display.textContent = text.slice(0, -1);
+        (Number.isNaN(runningOperation.op)) ? runningOperation.a = display.textContent : runningOperation.b = display.textContent;
+        if (text.length === 1) {
+            display.textContent = 0;
+            if (Number.isNaN(runningOperation.op))
+                disableOperatorButtons();
+        }
+        else
+            clearBtn.disabled = false;
+    }
     // If the entry erased was the decimal point, we can use it again
     if (text.charAt(text.length - 1) === ".")
         enableDecimalPoint();
@@ -150,29 +169,53 @@ function removeFromDisplay() {
  * display and the operation clicked (given in the argument)
  */
 function startOperation(op) {
-    const display = document.querySelector("#display");
-    // console.log(runningOperation.a);
-    // console.log(runningOperation.op);
-    if (op === "=") {
-        operate()
-    }
-    else {
-        runningOperation.op = op;
-        enableDecimalPoint();
-    }
+    runningOperation.op = op;
+    enableDecimalPoint();
 }
 
 /**
  * Ends an operation, which is when there was already
  * @param {*} op 
  */
-function endOperation(op) {
-    
+function endOperation(nextOp) {
+    const a = parseFloat(runningOperation.a);
+    const b = parseFloat(runningOperation.b);
+    const operation = runningOperation.op;
+    let res = operate(a, b, operation);
+    if (res !== "Crazy") {
+        res = parseFloat(operate(a, b, operation).toFixed(12))
+
+        disableEqualButton();
+        enableDecimalPoint();
+        console.log(res);
+
+        const display = document.querySelector("#display");
+        display.textContent = res;
+
+        if (nextOp === "=") {
+            runningOperation = {...EMPTY_OPERTATION};
+            runningOperation.a = res;
+            runningOperation.lastOp = "="; 
+        }
+        else {
+            runningOperation.a = res;
+            runningOperation.b = NaN;
+            runningOperation.op = nextOp;
+        }
+    }
+    else {
+        const display = document.querySelector("#display");
+        display.textContent = res;
+        disableOperatorButtons();
+        enableEraseButtons();
+        runningOperation = {...EMPTY_OPERTATION};
+    }
 }
 
 function eraseData() {
     enableDecimalPoint();
     runningOperation = {...EMPTY_OPERTATION};
+    const display = document.querySelector("#display");
     display.textContent = 0;
     disableOperatorButtons();
 }
@@ -214,4 +257,18 @@ function enableEqualButton() {
 function disableEqualButton() {
     const equalBtn = document.querySelector("#operator-eq");
     equalBtn.disabled = true;
+}
+
+function enableEraseButtons() {
+    const ce = document.querySelector(".clear");
+    const ac = document.querySelector("#operator-ac");
+    ce.disabled = false;
+    ac.disabled = false;
+}
+
+function disableEraseButtons() {
+    const ce = document.querySelector(".clear");
+    const ac = document.querySelector("#operator-ac");
+    ce.disabled = true;
+    ac.disabled = true;
 }
