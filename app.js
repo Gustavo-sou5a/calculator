3dbab89a341c33a8c0478b4acc8ccfe3a2d34e88
Gustavo-sou5a <gustavo.gomesdesousa@gmail.com>
2025-02-25 12:48:28 +0000

    Make the buttons from digits section work as intended

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ console.log(operate(3, 4, divide));
  */
 const NUM_DIGITS = 10;
 const OPERATORS = ["AC", "+", "-", "*", "/", "="];
+let decimalButtonActive = true;
 
 const digitsDiv = document.querySelector("#digits");
 for (let i = NUM_DIGITS-1; i >= 0; i--) {
@@ -37,19 +38,35 @@ for (let i = NUM_DIGITS-1; i >= 0; i--) {
     digitBtn.textContent = i;
     digitBtn.classList.add(`digit-${i}`);
     digitBtn.classList.add("digit");
+    digitBtn.addEventListener("click", () => addToDisplay(i));
     digitsDiv.appendChild(digitBtn);
 }
 
-// Adding the CE (Clear Entry) button to erase the last input
+// Adding the CE (Clear Entry) button for erasing the last input
 let clearBtn = document.createElement("button");
 clearBtn.textContent = "CE";
 clearBtn.classList.add("clear");
+clearBtn.addEventListener("click", () => {
+    const display = document.querySelector("#display");
+    let text = display.textContent;
+    display.textContent = text.slice(0, -1);
+    if (text.length === 1)
+        display.textContent = 0;
+    if (text.charAt(text.length - 1) === ".")
+        decimalButtonActive = true;
+});
 digitsDiv.insertBefore(clearBtn, document.querySelector(".digit-0"));
 
 // Adding the decimal button "." for float numbers
 let decimalBtn = document.createElement("button");
 decimalBtn.textContent = ".";
 decimalBtn.classList.add("decimal");
+decimalBtn.addEventListener("click", () => {
+    if (decimalButtonActive) {
+        addToDisplay(".");
+        decimalButtonActive = false;
+    }
+});
 digitsDiv.appendChild(decimalBtn);
 
 const operatorsDiv = document.querySelector("#operators");
@@ -57,10 +74,22 @@ OPERATORS.forEach((operator) => {
     let operatorBtn = document.createElement("button");
     operatorBtn.textContent = operator;
     operatorBtn.setAttribute("id", `operator-${operator}`);
-    if (operator !== "AC") {
+    if (operator !== "AC")
         operatorBtn.classList.add("operator");
-    }
+    // operatorBtn.addEventListener("click", () => {
+
+    // });
     operatorsDiv.appendChild(operatorBtn); 
 });
 
 
+/**
+ * Adding the number (or decimal point) to the display
+ */
+function addToDisplay(num) {
+    const display = document.querySelector("#display");
+    if (display.textContent === "0" && num !== ".")
+        display.textContent = num;
+    else
+        display.textContent += num;
+}
